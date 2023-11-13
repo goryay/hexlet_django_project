6985a7a43522d89141e347f5812abd27328cdba5
goryay <iam.goryn@gmail.com>
2023-11-13 21:17:33 +0300

    update article/urls

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.IndexView.as_view(), name='articles'),
+    path('<tags>/<int:article_id>/', views.index, name='article')
 ]
