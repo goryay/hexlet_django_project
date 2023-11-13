3f41010c980fdaec48080e311f949ba23270b00f
goryay <iam.goryn@gmail.com>
2023-11-13 21:00:26 +0300

    realize class indexview

--- views.py
+++ views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return HttpResponsw('article')
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
+
+
+def index(request, tags):
+    return render(request, 'article/index.html')
+
+def article(request):
+    tags = ['обучение', 'программирование', 'python', 'oop']
+    return render(
+        request,
+        'article.html',
+        context={'tags': tags},
+    )
